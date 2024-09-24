b357f34094730f62f91844f3fe8bce72ac2a3ca9
neddia <neddia1@gmail.com>
2024-09-23 21:47:26 -0400

    basic async functionality added

--- whatsminer/api.py
+++ whatsminer/api.py
@@ -19,6 +19,8 @@ from base64 import b64encode, b64decode
 from Crypto.Cipher import AES
 from passlib.hash import md5_crypt
 
+import asyncio
+
 logger = logging.getLogger(__name__)
 
 
@@ -69,32 +71,34 @@ class WhatsminerAccessToken:
 
         Final assembly: enc|base64(aes256("token,sign|set_led|auto", $aeskey))
         """
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((self.ip_address, self.port))
-            s.sendall('{"cmd": "get_token"}'.encode('utf-8'))
-            data = recv_all(s, 4000)
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((self.ip_address, self.port))
+                s.sendall('{"cmd": "get_token"}'.encode('utf-8'))
+                data = recv_all(s, 4000)
 
-        token_info = json.loads(data)["Msg"]
-        if token_info == "over max connect":
-            raise Exception(data)
+            token_info = json.loads(data)["Msg"]
+            if token_info == "over max connect":
+                raise Exception(data)
 
-        # Make the encrypted key from the admin password and the salt
-        pwd = crypt(self._admin_password, "$1$" + token_info["salt"] + '$')
-        pwd = pwd.split('$')
-        key = pwd[3]
+            # Make the encrypted key from the admin password and the salt
+            pwd = crypt(self._admin_password, "$1$" + token_info["salt"] + '$')
+            pwd = pwd.split('$')
+            key = pwd[3]
 
-        # Make the aeskey from the key computed above and prep the AES cipher
-        aeskey = hashlib.sha256(key.encode()).hexdigest()
-        aeskey = binascii.unhexlify(aeskey.encode())
-        self.cipher = AES.new(aeskey, AES.MODE_ECB)
+            # Make the aeskey from the key computed above and prep the AES cipher
+            aeskey = hashlib.sha256(key.encode()).hexdigest()
+            aeskey = binascii.unhexlify(aeskey.encode())
+            self.cipher = AES.new(aeskey, AES.MODE_ECB)
 
-        # Make the 'sign' that is passed in as 'token'
-        tmp = crypt(pwd[3] + token_info["time"], "$1$" + token_info["newsalt"] + '$')
-        tmp = tmp.split('$')
-        self.sign = tmp[3]
+            # Make the 'sign' that is passed in as 'token'
+            tmp = crypt(pwd[3] + token_info["time"], "$1$" + token_info["newsalt"] + '$')
+            tmp = tmp.split('$')
+            self.sign = tmp[3]
 
-        self.created = datetime.datetime.now()
-
+            self.created = datetime.datetime.now()
+        except Exception as e:
+            logger.error(f"Failed to initialize write access: {e}")
 
     def enable_write_access(self, admin_password: str):
         self._admin_password = admin_password
@@ -113,91 +117,166 @@ class WhatsminerAccessToken:
         return True
 
 
-
 class WhatsminerAPI:
-    """ Stateless classmethod-only read/write API calls. Use a WhatsminerAccessToken
-        instance for each ASIC you want to access.
-    """
-
     @classmethod
-    def get_read_only_info(self, access_token: WhatsminerAccessToken, cmd: str, additional_params: dict = None):
-        """ Send READ-ONLY API command.
+    async def get_multiple_read_only_info(cls, access_token: WhatsminerAccessToken, commands: list):
+        reader = None
+        writer = None
+        try:
+            reader, writer = await asyncio.wait_for(asyncio.open_connection(access_token.ip_address, access_token.port), 5)
+            results = []
+            for cmd in commands:
+                json_cmd = {"cmd": cmd}
+                writer.write(json.dumps(json_cmd).encode('utf-8') + b'\n')
+                await writer.drain()
+                
+                data = await asyncio.wait_for(cls.recv_all(reader), 5)
+                if data:
+                    try:
+                        result = json.loads(data.decode())
+                        results.append(result)
+                    except json.JSONDecodeError:
+                        results.append({"error": f"Invalid JSON for command {cmd}", "raw_data": data.decode()})
+                else:
+                    results.append({"error": f"No data received for command {cmd}"})
+                
+                # Add a small delay between commands
+                await asyncio.sleep(0.1)
+            
+            return results
+        finally:
+            if writer:
+                writer.close()
+                await writer.wait_closed()
 
-            e.g. WhatsminerAPI.get_read_only_info(access_token, cmd="summary")
-
-            Returns: json response
+    @staticmethod
+    async def recv_all(reader):
+        data = bytearray()
+        while True:
+            try:
+                chunk = await reader.read(1024)
+                if not chunk:
+                    break
+                data.extend(chunk)
+                if b'\n' in chunk:
+                    break
+            except asyncio.TimeoutError:
+                break
+        return bytes(data)
+    
+    @classmethod
+    async def get_read_only_info(cls, access_token: WhatsminerAccessToken, cmd: str, additional_params: dict = None):
+        """ Send READ-ONLY API command asynchronously.
         """
         json_cmd = {"cmd": cmd}
         if additional_params:
             json_cmd.update(additional_params)
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((access_token.ip_address, access_token.port))
-            s.send(json.dumps(json_cmd).encode('utf-8'))
-            data = recv_all(s, 4000)
-
+        reader = None
+        writer = None
         try:
-            return json.loads(data.decode())
-        except Exception as e:
-            logging.exception("Error calling read-only endpoint")
+            # Attempt to open a connection with a timeout
+            reader, writer = await asyncio.wait_for(asyncio.open_connection(access_token.ip_address, access_token.port), 5)
+            writer.write(json.dumps(json_cmd).encode('utf-8'))
+            await writer.drain()
+            # Data reception with a timeout
+            data = await asyncio.wait_for(cls.recv_all(reader, 4000), 5)
             try:
-                logging.error(data.decode())
-            except:
-                pass
+                return json.loads(data.decode())
+            except json.JSONDecodeError:
+                raise ValueError("Received data is not valid JSON")
+        except asyncio.TimeoutError as e:
+            raise asyncio.TimeoutError(f"Timeout occurred: {str(e)}") from None
+        finally:
+            if writer is not None:
+                writer.close()
+                await writer.wait_closed()
+            if reader is not None:
+                await reader.read(0)  # Recommended way to close the StreamReader
 
-            raise e
+
 
 
     @classmethod
-    def exec_command(self, access_token: WhatsminerAccessToken, cmd: str, additional_params: dict = None):
-        """ Send WRITEABLE API command.
+    async def get_multiple_read_only_info(cls, access_token: WhatsminerAccessToken, commands: list):
+        reader = None
+        writer = None
+        try:
+            reader, writer = await asyncio.wait_for(asyncio.open_connection(access_token.ip_address, access_token.port), 5)
+            results = []
+            for cmd in commands:
+                json_cmd = {"cmd": cmd}
+                writer.write(json.dumps(json_cmd).encode('utf-8'))
+                await writer.drain()
+                data = await asyncio.wait_for(cls.recv_all(reader, 4000), 5)
+                results.append(json.loads(data.decode()))
+            return results
+        finally:
+            if writer is not None:
+                writer.close()
+                await writer.wait_closed()
+            if reader is not None:
+                await reader.read(0)
 
-            e.g. WhatsminerAPI.exec_command(access_token, cmd="power_off", additional_params={"respbefore": "true"})
+
+    @classmethod
+    async def exec_command(cls, access_token: WhatsminerAccessToken, cmd: str, additional_params: dict = None):
+        """ Send WRITEABLE API command asynchronously.
+
+            e.g. await WhatsminerAPI.exec_command(access_token, cmd="power_off", additional_params={"respbefore": "true"})
 
             Returns: json response
         """
         if not access_token.has_write_access():
             raise Exception("access_token must have write access")
 
-        # Assemble the plaintext json
-        json_cmd = {"cmd": cmd, "token": access_token.sign}
-        if additional_params:
-            json_cmd.update(additional_params)
-        api_cmd = json.dumps(json_cmd)
-
         # Encrypt it and assemble the transport json
-        enc_str = str(
-            base64.encodebytes(
-                access_token.cipher.encrypt(add_to_16(api_cmd))),
-                encoding='utf8'
-            ).replace('\n', '')
-        data_enc = {'enc': 1}    # transmit w/ "enc" to signal that it's encrypted
-        data_enc['data'] = enc_str
-        api_packet_str = json.dumps(data_enc)
+        enc_str = base64.encodebytes(access_token.cipher.encrypt(cls.add_to_16(json.dumps({
+            "cmd": cmd, "token": access_token.sign, **(additional_params or {})
+        })))).decode('utf8').replace('\n', '')
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((access_token.ip_address, access_token.port))
-            s.send(api_packet_str.encode())
-            data = recv_all(s, 4000)
+        api_packet_str = json.dumps({'enc': 1, 'data': enc_str})
 
+
+        reader, writer = await asyncio.wait_for(asyncio.open_connection(access_token.ip_address, access_token.port), 5)  # 5-second timeout
         try:
+            writer.write(api_packet_str.encode())
+            await writer.drain()
+            data = await asyncio.wait_for(cls.recv_all(reader, 4000), 5)  # 5-second timeout
             json_response = json.loads(data.decode())
+
+
             if "STATUS" in json_response and json_response["STATUS"] == "E":
                 logger.error(json_response["Msg"])
-                raise Exception(api_cmd + "\n" + json_response["Msg"])
+                raise Exception(json_response["Msg"])
 
-            resp_ciphertext = b64decode(json.loads(data.decode())["enc"])
-            resp_plaintext = access_token.cipher.decrypt(resp_ciphertext).decode().split("\x00")[0]
+            resp_ciphertext = base64.b64decode(json_response["enc"])
+            resp_plaintext = access_token.cipher.decrypt(resp_ciphertext).decode().rstrip("\x00")
             resp = json.loads(resp_plaintext)
+            return resp
+        except asyncio.TimeoutError:
+            logger.exception("Connection timed out")
+            raise
         except Exception as e:
             logger.exception("Error decoding encrypted response")
-            try:
-                logger.error(data.decode())
-            except:
-                pass
             raise e
+        finally:
+            writer.close()
+            await writer.wait_closed()
 
-        return resp
+    @staticmethod
+    async def recv_all(reader, n):
+        data = bytearray()
+        while len(data) < n:
+            packet = await reader.read(n - len(data))
+            if not packet:
+                break
+            data.extend(packet)
+        return data
+
+    @staticmethod
+    def add_to_16(s):
+        return str.encode(s + '\0' * ((16 - len(s) % 16) % 16))
 
 
 
@@ -210,6 +289,7 @@ def crypt(word, salt):
     extra_str = match.group(2)
     result = md5_crypt.hash(word, salt=extra_str)
     return result
+
 
 
 # Adapted from: https://stackoverflow.com/a/17668009
@@ -225,10 +305,3 @@ def recv_all(sock, n):
             return None
         data.extend(packet)
     return data
-
-
-def add_to_16(s):
-    while len(s) % 16 != 0:
-        s += '\0'
-    return str.encode(s)  # return bytes
-
